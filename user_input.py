c4a2c7b085a03ee8d84bda51b1bcef7a1fe907b7
Joel Masiaga <masiagajoel001@gmail.com>
2024-03-20 18:00:21 +0300

    First Commit

--- user_input.py
+++ user_input.py
@@ -2,4 +2,4 @@ name = input("What is your name?: ")
 age = input("How old are you?: ")
 location = input("Where do you live?: ")
 
-print("Hello "+name+". You are "+age+" old and you live in "+location)
+print("Hello "+name+". You are "+age+" years old and you live in "+location)
